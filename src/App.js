7ffa9fd16bfa65269f71dcb047bacb1d0deee3d0
unknown <joannasmoura@gmail.com>
2019-10-03 17:40:03 -0300

    correção

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 import GlobalStyles from './styles/global';
 import Header from './components/Header';
-import Routes from "./routes";
+import Routes from './routes';
 
 function App() {
   return (
