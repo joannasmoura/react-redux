10c8972c9a69403f7cb9a72a76bca0107be1cb84
unknown <joannasmoura@gmail.com>
2019-10-04 13:19:09 -0300

    adição de redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import logo from '../../assets/images/logo.svg'
+import { Link } from 'react-router-dom';
+import {MdShoppingBasket} from 'react-icons/md';
+import { Container, Cart } from './styles';
+import { connect } from 'react-redux';
 
-import { Container } from './styles';
-
-export default function Header() {
-  return (
-    <Container>
-      <img src={logo} alt="RocketShoes"/>
-    </Container>
-  );
+ function Header({cartSize}) {
+    return (
+        <Container>
+            <Link to="/">
+                <img src={logo} alt="RocketShoes" />
+            </Link>
+            <Cart to="/cart">
+                <div>
+                    <strong>Meu carrinho</strong>
+                    <span>{cartSize} itens</span>
+                </div>
+                <MdShoppingBasket size={36} color="#FFF"/>
+            </Cart>
+        </Container>
+    );
 }
+
+export default connect(state => ({
+    cartSize: state.cart.length,
+}))(Header);
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,8 +1,60 @@
 import React from 'react';
+import {MdRemoveCircleOutline, MdAddCircleOutline, MdDelete} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart(){  
-    return <div />;
+    return (
+        <Container>
+            <ProductTable>  
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>PRODUTO</th>
+                        <th>QUANTIDADE</th>
+                        <th>SUBTOTAL</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/36/HZM-1731-036/HZM-1731-036_detalhe2.jpg?ims=326x" alt="Tênis"/>
+                        </td>
+                        <td>
+                            <strong>Tênis Nike</strong>
+                            <span>R$279.90</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                                </button>
+                                <input type="number" readonly value={2} />
+                                <button type="button">
+                                    <MdAddCircleOutline size={20} color="#7159c1" />
+                                </button>   
+                            </div>                            
+                        </td>
+                        <td>
+                            <strong>R$559.80</strong>
+                        </td>
+                        <td>
+                            <button type="button"><MdDelete size={20} color="#7159c1"/></button>
+                        </td>
+                    </tr>
+                </tbody>
+
+            </ProductTable>
+
+            <footer>
+                <button type="button">Finalizar Pedido</button>
+                <Total>
+                    <span>TOTAL</span>
+                    <strong>R$1920.27</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
   
 }
